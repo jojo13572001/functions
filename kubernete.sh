d71c6298202f42c4d3d79ba406c116e670d4189d
bean <jojo13572001@yahoo.com.tw>
2017-07-28 11:14:20 +0800

    add port forwarding support

--- kubernete.sh
+++ kubernete.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 ##delete all services,pods
 kubectl delete --force -f kubernete.yaml
+sleep 5 
 kubectl create -f kubernete.yaml
+sleep 5
+podname="$(kubectl get --no-headers=true pods -o name | awk -F "/" '{print $2}')"
+kubectl port-forward ${podname} 8080:8080
